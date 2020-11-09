be6635e0df0f1d73270891119f909977adde06f7
Anna Chen <106anna@gmail.com>
2020-11-08 20:32:48 -0800

    Changed strtok_c implementation to not copying the string to tokenize

--- P3/prog3.c
+++ P3/prog3.c
@@ -2,9 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <limits.h>
-
-// check memory leaks with valgrind on CSIF
 
 int parseForHighest(const char* filename, int* highest) {
     if (!filename || !highest) return -1;
@@ -55,33 +52,35 @@ int* getAllHigherThan(const int* arr, unsigned arrlen, int threshold, unsigned* 
 }
 
 char* strtok_c(const char* str, const char* delim) {
-    static char strToSearch[INT_MAX] = "";
     static const char* searchPtr = NULL;
+    static int maxLen = 0; // length of original string to tokenize
+    static int currIndex = 0;
     if (str) {
-        strcpy(strToSearch, str);
-        searchPtr = &strToSearch[0];
+        maxLen = strlen(str);
+        currIndex = 0;
+        searchPtr = &str[currIndex];
     }
-    if (!searchPtr || !strlen(searchPtr)) { // if first call has str as NULL or no more to search
+    // if (!searchPtr || !strlen(searchPtr)) { // if first call has str as NULL or no more to search
+    //     return NULL;
+    // }
+    if (currIndex >= maxLen) {
         return NULL;
     }
     int tempIndex = 0;
     int isStart = 1; // flag for finding non-delimiter to start at
     char* tempTok = calloc(strlen(searchPtr) + 1, sizeof(char));
-    while (searchPtr[0]) {
-        for (int i = 0; i < strlen(delim); i++) {
-            if (searchPtr[0] == delim[i]) {
-                if (isStart) {
-                    searchPtr++;
-                    goto nextChar;
-                }
-                searchPtr++;
+    for (int i = currIndex; i < maxLen; i++) {
+        for (int j = 0; j < strlen(delim); j++) {
+            if (searchPtr[i] == delim[j]) {
+                if (isStart) goto nextChar;
+                currIndex++;
                 goto exitLoop;
             }
         }
         if (isStart) isStart = 0;
-        tempTok[tempIndex++] = searchPtr[0];
-        searchPtr++;
-        nextChar: ;
+        tempTok[tempIndex++] = searchPtr[currIndex];
+        nextChar:
+        currIndex++;
     }
     exitLoop:
     if (!strlen(tempTok)) {
