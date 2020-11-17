cdd86103acc4b7139734fa11afbb79e17787cf11
Anna Chen <106anna@gmail.com>
2020-11-16 17:35:21 -0800

    Part 1 library.c own tests pass

--- P4/library.c
+++ P4/library.c
@@ -20,34 +20,27 @@ struct Library* loadLibrary(const char* filename) {
     if (!fp) return NULL;
 
     char buf[BUF_LEN];
-    fgets(buf, MAX_LINE_LEN, fp); // buf contains # of books after this
+    fgets(buf, MAX_LINE_LEN, fp); // buf contains # of books after this (first line in filename)
     struct Library* libraryPtr = (struct Library*)malloc(sizeof(struct Library));
     libraryPtr->numBooks = atoi(buf);
     libraryPtr->books = malloc(libraryPtr->numBooks * sizeof(struct Book));
 
     int bookIndex = 0;
     while (fgets(buf, MAX_LINE_LEN, fp)) { // read in filename until nothing left to read
-        // char *title, *author;
-        // int year;
         // filename is a CSV file -> tokenize by comma
         struct Book book; // create instances of Book and store in books attribute of libraryPtr (books = array of Book instances)
         for (int i = 0; i < 3; i++) {
             char* tok = i == 0 ? strtok(buf, ",") : strtok(NULL, ",");
             switch (i) {
                 case 0:
-                    // title = calloc(strlen(tok) + 1, sizeof(char));
-                    // strcpy(title, tok);
                     book.title = calloc(strlen(tok) + 1, sizeof(char));
                     strcpy(book.title, tok);
                     break;
                 case 1:
-                    // author = calloc(strlen(tok) + 1, sizeof(char));
-                    // strcpy(author, tok);
                     book.author = calloc(strlen(tok) + 1, sizeof(char));
                     strcpy(book.author, tok);
                     break;
                 case 2:
-                    // year = atoi(strtok(tok, "\n")); // strip newline character of year token
                     book.year = atoi(strtok(tok, "\n")); // strip newline character of year token
                     break;
             }
@@ -59,8 +52,7 @@ struct Library* loadLibrary(const char* filename) {
 }
 
 /**
- * Prints out each Book's details using the array of Books that's an attribute
- * of Library.
+ * Prints out each book's details (title, author, year) in a given library.
  * 
  * @param library References a Library instance
  * @return 0 if library is NULL; 1 otherwise
@@ -76,14 +68,14 @@ int printLibrary(const struct Library* library) {
     return 1;
 }
 
-// Assume the same title never appears twice in a given file
 /**
  * Counts the number of common titles between two Library instances. This function 
  * assumes that the same title never appears twice in a given file.
  * 
  * @param lib1 Reference to first Library instance
  * @param lib2 Reference to second Library instance
- * @return 0 if either lib1 or lib2 is NULL; number of common titles otherwise
+ * @return 0 if either lib1 or lib2 is NULL; otherise, the number of common titles 
+ * between lib1 and lib2
  */
 int countCommonTitles(const struct Library* lib1, const struct Library* lib2) {
     if (!lib1 || !lib2) return 0;
