211bcae697052065410bbc1bf81622278b3a48f1
Anna Chen <106anna@gmail.com>
2020-12-15 11:43:58 -0800

    Cleaned up print_view.cpp

--- P6/Provided_Code_Files/print_view.cpp
+++ P6/Provided_Code_Files/print_view.cpp
@@ -18,17 +18,6 @@ PrintView::~PrintView()
 void PrintView::draw(const std::vector<std::string>& lines)
 {
     // TODO: Implement.
-    // for (int row = -1; row < mHeight+1; row++) {
-    //     for (int col = -1; col < mWidth+1; col++) {
-    //         if (row == -1 || row == mHeight || col == -1 || col == mWidth) {
-    //             std::cout << '*';
-    //         }
-    //         else {
-    //             std::cout << lines[row][col];
-    //         }
-    //     }
-    //     std::cout << std::endl;
-    // }
     std::string border = std::string(mWidth+2, '*');
     std::cout << border << std::endl;
     for (size_t i = 0; i < lines.size(); i++) {
